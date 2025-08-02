4124b4093750a3c599ee010aa66c428983d8a34b
rash-abdo <abdelrahman224571@example.com>
2025-08-02 04:15:04 +0300

    made some fixes to the userContoller, moved jwt.js to the utillities dir

--- services/userService.js
+++ services/userService.js
@@ -13,12 +13,27 @@ exports.register = async ({username, password, admin}) => {
     await userRepo.createUser(username, hashed, admin)
 }
 
-exports.login = async ({ username, password }) => {
+exports.login = async ({ username, password },req,res) => {
     const user = await userRepo.findByUsername(username)
     if (!user) throw new Error('unregistered user')
 
     const match = await bcrypt.compare(password, user.password)
     if (!match) throw new Error('wrong password')
     
-    return {id:user.id, username:user.username, admin:user.admin}
+    user = {id:user.id, username:user.username, admin:user.admin}
+
+    const accessToken = jwt.generateAccessToken(user)
+    const refreshToken = jwt.generateRefreshToken(user)
+    res.cookie('refreshToken', refreshToken, {
+        httpOnly: true,
+        maxAge: 24*60*60*1000
+    })
+    res.json({message:'Login successful',
+        accessToken})
+}
+
+exports.logout = async (req,res) => {
+    res.clearCookie('refreshToken',{
+        httpOnly:true,
+    })
 }
